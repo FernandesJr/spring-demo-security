0f00bc47e5023adda2b7e9cd9469882a6e54b0a5
Fernandes Júnior <fernaandes.jr@gmail.com>
2021-10-12 13:30:00 -0300

    Send email

--- src/main/java/com/fernandes/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/fernandes/curso/security/DemoSecurityApplication.java
@@ -1,14 +1,37 @@
 package com.fernandes.curso.security;
 
+import com.fernandes.curso.security.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		//System.out.println(new BCryptPasswordEncoder().encode("123"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+	@Autowired
+	private JavaMailSender sender;
+
+	@Autowired
+	private EmailService emailService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		/*SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("junior.fernandes@mcctecnologia.com.br");
+		message.setSubject("Testando SpringBoot email");
+		message.setText("Lindão deu certo");
+		sender.send(message);
+		System.out.println("Tentei enviar");*/
+
+		emailService.enviarPedidoConfirmacaoCadastro("junior.fernandes@mcctecnologia.com.br","sad54f");
 	}
 }
---
+++ src/main/java/com/fernandes/curso/security/service/EmailService.java
@@ -0,0 +1,50 @@
+package com.fernandes.curso.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private SpringTemplateEngine template; //Thymeleaf
+
+    public void enviarPedidoConfirmacaoCadastro(String destino, String codigo) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper =
+                new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8");
+
+        //Adicionando variáveis a página que será enviada
+        Context context = new Context();
+        context.setVariable("titulo", "Bem vindo a clínica Spring Security");
+        context.setVariable("texto", "Precisamos que confirme seu cadastro.");
+        context.setVariable("linkConfirmacao",
+                "http://localhost:8080/u/confirmacao/cadastro?codigo=" + codigo);
+
+        String html = template.process("email/confirmacao", context);
+
+        helper.setTo(destino);
+        helper.setFrom("nao-responder@clinica.com"); //O From é setado pelo o application.properties. Alguns servidores iram mostrar esse email como uma espécie de máscara
+        helper.setSubject("Confirmação de cadastro");
+        helper.setText(html, true);
+
+        //Lembrete sempre colocar as img por último
+        helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png")); //importante! projeto KAIZEN
+
+        mailSender.send(message);
+
+    }
+
+}
